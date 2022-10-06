6caff4aa7db2228817e974833d9574d5e1d39388
newwade <abhilashwithh@gmail.com>
2022-10-06 09:17:06 +0530

    OCT 6 22

---
+++ shell/shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# variable declaration
+# name="billy"
+# echo $name
+
+#conditional statement
+
+# if grep "127.0.0.1" /etc/hosts; then
+#     echo "OK"
+# else
+#     echo "ERROR"
+# fi
+
